74bec606b19f543e12caf24671dcd7a909cad210
Kevin <84540535+luminerrr@users.noreply.github.com>
2022-05-20 13:24:22 +0700

    trying useEffect hook using placeholder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,6 +10,28 @@ function App() {
       return currentCount + 1; 
     })
   }
+
+  // function getRandomInt(min, max){
+  //   min = Math.ceil(min);
+  //   max = Math.floor(max);
+  //   return Math.floor(Math.random() * (max - min) + min);
+  // }
+
+  // const id = getRandomInt(10, 100);
+  
+
+  
+  const [todo, setTodo] = useState([]);
+  const style = {textDecoration: todo?.completed ? "line-through" : "unset"};
+
+  useEffect(()=>{
+    fetch(`https://jsonplaceholder.typicode.com/todos/${count}`)
+    .then((response) => response.json())
+    .then((data) => setTodo(data))
+    .catch((err)=> console.log(err));
+  }, [count]);
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -21,14 +43,16 @@ function App() {
         <button className="App-button" onClick={increase}>
           click me!
         </button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {!!todo ? (
+          <div>
+            <h1 style={style}>{todo.title}</h1>
+          </div>
+        ) : (
+          <div>
+            <h1>Loading...</h1>
+          </div>
+        )}
+        <h1>{todo.title}</h1>        
       </header>
     </div>
   );
